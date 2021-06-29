4018ffe9c4345f1bf586e94a97860eac65733b92
Joko Supriyanto <jokomanza@gmail.com>
2021-06-29 10:57:02 +0700

    delete error view

--- application/views/errors/html/error_exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-echo json_encode(['error' => array(
-	'code' => '500',
-	'type' => get_class($exception),
-	'user_message' => $message,
-	'develoer_message' => $exception->getFile() . " in line " . $exception->getLine()
-)]);
--- application/views/errors/html/error_php.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-echo json_encode(['error' => array(
-	'code' => '500',
-	'type' => 'PHP Error',
-	'user_message' => $message,
-	'develoer_message' => $filepath . " in line " . $line
-)]);
